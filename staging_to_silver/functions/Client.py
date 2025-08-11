24ae0d3f951c83cb0a68032d0f06ab1b3e1cb110
JoostOdz <j.barink@oldenzaal.nl>
2025-08-11 14:33:57 +0200

    Create Client.py

---
+++ staging_to_silver/functions/Client.py
@@ -0,0 +1,34 @@
+# staging_to_silver/functions/queries.py
+from sqlalchemy import MetaData, select, and_, or_, func, literal, case
+
+def CLIENT(engine, source_schema=None):
+    table_names = ["SZCLIENT"]
+
+    metadata = MetaData()
+    metadata.reflect(bind=engine,
+                     schema=source_schema,
+                     only=table_names)
+
+    # grab them (SQLAlchemy keys them as f"{schema}.{name}")
+    fq_names = [f"{source_schema + '.' if source_schema else ''}{n}" for n in table_names]
+    szclient = (
+        metadata.tables[name] for name in fq_names
+    )
+
+    return (
+        select(
+            szclient.c.CLIENTNR.label("RECHTSPERSOON_ID"),
+            szclient.c.IND_GEZAG.label("GEZAGSDRAGERGEKEND_ENUM_ID"),
+            cast(literal(None), String(80)).label("CODE"),
+            cast(literal(None), String(80)).label("JURIDISCHESTATUS"),
+            cast(literal(None), String(80)).label("WETTELIJKEVERTEGENWOORDIGING"),
+       
+        )
+           
+        .select_from(szclient)
+    )
+            
+
+queries = {
+    'CLIENT': CLIENT
+}
