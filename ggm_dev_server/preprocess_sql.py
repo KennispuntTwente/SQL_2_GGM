5989fca54782caf112c22e85d5a0def98b00a089
lukakoning <koningluka@gmail.com>
2025-10-22 15:30:02 +0200

    Remove unneeded leftover files

--- ggm_dev_server/preprocess_sql.py
+++
@@ -1,9 +0,0 @@
-"""
-Backwards-compat shim for the historical `ggm_dev_server.preprocess_sql`.
-
-Use `dev_sql_server.preprocess_sql` instead.
-"""
-
-from __future__ import annotations
-
-from dev_sql_server.preprocess_sql import *  # noqa: F401,F403 re-export
